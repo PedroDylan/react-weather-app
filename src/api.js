31d9d857fd06d991ae8e73e4ec5524e4dc53efd1
Pedro Dylan <pedrodylan3@gmail.com>
2023-05-15 20:31:47 -0300

    fixing the last god damm commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?';
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {  
+		'X-RapidAPI-Key': '8c8d28580emsh530f3a487792da8p199597jsn5306456e5bf1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
